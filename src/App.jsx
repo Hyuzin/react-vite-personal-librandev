1a45c4cff486fffe27c6def09cf7f7801322e8fe
Hyuzin <muhammad.libran1@gmail.com>
2023-06-04 12:12:12 +0700

    React Template for my Personal Use.

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <h1 className="text-3xl font-semibold">HELLO IM REACT!</h1>
+    </div>
+  );
+};
+
+export default App;
